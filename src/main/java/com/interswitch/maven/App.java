8abc8c975f26c12f03ab1b177ed78023423eccd4
Salome Odusoro <salome.odusoro@interswitchgroup.com>
2023-08-19 09:34:30 +0100

    new feature added to simple calculator, upper-lower-case converter

--- src/main/java/com/interswitch/maven/App.java
+++ src/main/java/com/interswitch/maven/App.java
@@ -6,6 +6,12 @@ package com.interswitch.maven;
  */
 public class App {
     public static void main(String[] args) {
+        UpperLowerCase upperLowerCase = new UpperLowerCase();
+
         System.out.println("good-day");
+        System.out.println("To upper case: ");
+        System.out.printf("%s to %s: \n", "Hello World", upperLowerCase.wordToUpperCase("Hello World"));
+        System.out.println("To lower case: ");
+        System.out.printf("%s to %s: \n", "Hello World", upperLowerCase.wordToLowerCase("Hello World"));
     }
 }
